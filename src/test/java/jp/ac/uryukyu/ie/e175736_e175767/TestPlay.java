8e1ff3315cb6e74bedcd8a3fc0d25d47eea9362e
Motomu Tamaki <e175736@ie.u-ryukyu.ac.jp>
2019-01-22 07:26:09 +0900

    report6

---
+++ src/test/java/jp/ac/uryukyu/ie/e175736_e175767/TestPlay.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e175736_e175767;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+class TestPlay {
+
+    @Test
+    void changeKomaColor() {
+        Field field = new Field();
+        field.changeStoneColor(4, 4, "W");
+        assertEquals("W", field.getKoma(4,4).getColor());
+    }
+}
