95231d502a0ad49ee602d0f59b07238a472490f7
Motomu Tamaki <e175736@ie.u-ryukyu.ac.jp>
2019-01-22 07:10:05 +0900

    report6

---
+++ src/main/java/jp/ac/uryukyu/ie/e175736_e175767/Play.java
@@ -0,0 +1,33 @@
+package jp.ac.uryukyu.ie.e175736_e175767;
+
+import java.util.Scanner;
+
+public class Play {
+    public int x;
+    public int y;
+
+    public Play(){
+        get_user_input();
+    }
+
+    /**
+     * プレイヤーの入力した座標を取得するためのメソッド。
+     */
+    public void get_user_input(){
+        try {
+            System.out.println("置きたいマスの座標を入力してください。：x-y");
+            Scanner user_input = new Scanner(System.in);
+            String str = user_input.nextLine();
+            //"-"で区切る
+            String[] str_list = str.split("-");
+            this.x = Integer.parseInt(str_list[0]);
+            this.y = Integer.parseInt(str_list[1]);
+        }catch (NumberFormatException e){
+            System.out.println("不正な入力です。");
+            get_user_input();
+        }catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("不正な入力です。");
+            get_user_input();
+        }
+    }
+}
